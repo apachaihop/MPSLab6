e890c850a1ac17f51b447c790ea98d665434c365
apachaihop <poizen211@gmail.com>
2024-05-04 11:40:16 +0300

    Добавьте файлы проекта.

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(559, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Записать Все";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(477, 320);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(667, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Фильтровать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(667, 301);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(564, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Поле для фильтра";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(667, 328);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(667, 355);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(564, 334);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Минимум";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(564, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Максимум";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(567, 389);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Равно";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(667, 381);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(559, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Сортировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection StrCon = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\down;Extended Properties=text");
+        private void Form1_Load_1(object sender, EventArgs e)
+        { //Определяем подключение
+            
+            //Строка для выборки данных
+            string Select1 = "SELECT * FROM [Dictionary1.txt]";
+            //Создание объекта Command
+            OleDbCommand comand1 = new OleDbCommand(Select1, StrCon);
+            //Определяем объект Adapter для взаимодействия с источником данных
+            OleDbDataAdapter adapter1 = new OleDbDataAdapter(comand1);
+            //Определяем объект DataSet
+            DataSet AllTables = new DataSet();
+            //Открываем подключение
+            StrCon.Open();
+            //Заполняем DataSet таблицей из источника данных
+            adapter1.Fill(AllTables);
+            //Заполняем обект datagridview для отображения данных на форме
+            dataGridView1.DataSource = AllTables.Tables[0];
+            StrCon.Close();
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                comboBox1.Items.Add(column.Name);
+            }
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(@"C:\down\Dictionary1.txt", false, Encoding.Default);
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    sw.Write(dataGridView1.Columns[j].HeaderText);
+                    if (j < dataGridView1.ColumnCount - 1)
+                        sw.Write(",");
+                }
+                sw.WriteLine();
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                    {
+                        sw.Write(dataGridView1.Rows[i].Cells[j].Value);
+                        if (j < dataGridView1.ColumnCount - 1)
+                            sw.Write(",");
+                    }
+                    sw.WriteLine();
+                }
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void UpdateRecord(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получите ID записи, которую нужно обновить
+                string idToUpdate = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+
+                // Запрос на обновление
+                string updateQuery = $"UPDATE [Dictionary1.txt] SET column1 = value1, column2 = value2 WHERE id = {idToUpdate}";
+
+                // Выполните запрос обновления
+                OleDbCommand updateCommand = new OleDbCommand(updateQuery, StrCon);
+                StrCon.Open();
+                updateCommand.ExecuteNonQuery();
+                StrCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DeleteRecord(object sender, EventArgs e)
+        {
+            try
+            {
+                // Получите ID записи, которую нужно удалить
+                string idToDelete = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+
+                // Запрос на удаление
+                string deleteQuery = $"DELETE FROM [Dictionary1.txt] WHERE id = {idToDelete}";
+
+                // Выполните запрос на удаление
+                OleDbCommand deleteCommand = new OleDbCommand(deleteQuery, StrCon);
+                StrCon.Open();
+                deleteCommand.ExecuteNonQuery();
+                StrCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void FilterRecords(object sender, EventArgs e)
+        {
+            try
+            {
+                // Запрос на фильтрацию
+                string filterQuery = "SELECT * FROM [Dictionary1.txt] WHERE column1 = value1";
+
+                // Выполните запрос фильтрации и обновите dataGridView
+                OleDbCommand filterCommand = new OleDbCommand(filterQuery, StrCon);
+                OleDbDataAdapter filterAdapter = new OleDbDataAdapter(filterCommand);
+                DataSet filterDataSet = new DataSet();
+                StrCon.Open();
+                filterAdapter.Fill(filterDataSet);
+                dataGridView1.DataSource = filterDataSet.Tables[0];
+                StrCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string idToDelete = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+
+                string deleteQuery = $"DELETE FROM [Dictionary1.txt] WHERE id = {idToDelete}";
+
+                // Выполните запрос на удаление
+                OleDbCommand deleteCommand = new OleDbCommand(deleteQuery, StrCon);
+                StrCon.Open();
+                deleteCommand.ExecuteNonQuery();
+                StrCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            string columnName = comboBox1.Text;
+            string filterValue = textBox1.Text;
+
+            // Применяем фильтр
+            FilterData(columnName, filterValue,"");
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void FilterData(string columnName, string filterValueMin,string filterValueMax)
+        {
+            // Создаем новый DataSet для хранения отфильтрованных данных
+            DataSet filteredData = new DataSet();
+
+            // Открываем подключение
+            StrCon.Open();
+
+            // Строка для выборки отфильтрованных данных
+            string selectQuery = $"SELECT * FROM [Dictionary1.txt] WHERE [{columnName}] > {filterValueMin}";
+
+            // Создаем объект Command
+            OleDbCommand command = new OleDbCommand(selectQuery, StrCon);
+
+            // Определяем объект Adapter для взаимодействия с источником данных
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+
+            // Заполняем DataSet отфильтрованной таблицей из источника данных
+            adapter.Fill(filteredData);
+
+            // Заполняем объект DataGridView для отображения данных на форме
+            dataGridView1.DataSource = filteredData.Tables[0];
+
+            // Закрываем подключение
+            StrCon.Close();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
